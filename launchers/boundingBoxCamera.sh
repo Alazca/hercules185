623e0894967eecaed3e5560b96cee723ec2d2ca0
Alazca <andylazcanop@gmail.com>
2024-11-30 22:55:12 -0800

    testing sourcing inside launcher

--- launchers/boundingBoxCamera.sh
+++ launchers/boundingBoxCamera.sh
@@ -4,7 +4,7 @@ source /environment.sh
 
 # initialize launch file
 dt-launchfile-init
-source /opt/ros/<distro>/setup.bash
+source /opt/ros/noetic/setup.bash
 source ~/catkin_ws/devel/setup.bash
 # launch publisher
 dt-exec rosrun vision_package boundingBoxCamera.py
