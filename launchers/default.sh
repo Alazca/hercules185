ba1d719b30347514c50d491c66f9c696d1394ad5
Alazca <andylazcanop@gmail.com>
2024-12-01 01:34:12 -0800

    experimenting with removing unnecessary exec in launcher

--- launchers/default.sh
+++ launchers/default.sh
@@ -15,7 +15,7 @@ dt-launchfile-init
 # launching app
 dt-exec echo "Starting Packages - - -"
 
-#dt-exec roslaunch vision_package boundingBoxCamera.launch
+dt-exec roslaunch vision_package boundingBoxCamera.launch
 
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
