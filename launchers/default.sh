48012e5d979ee9880629fa81c1f61d9703d99335
Alazca <andylazcanop@gmail.com>
2024-12-01 13:13:44 -0800

    changed permissions for new launch scripts

--- launchers/default.sh
+++ launchers/default.sh
@@ -14,8 +14,7 @@ dt-launchfile-init
 
 # launching app
 dt-exec echo "Starting Packages - - -"
-#dt-exec roslaunch vision_package boundingBoxCamera.launch
-dt-exec roslaunch vision_package raw_boundingBox.launch
+dt-exec roslaunch vision_package boundingBoxCamera.launch
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
 
